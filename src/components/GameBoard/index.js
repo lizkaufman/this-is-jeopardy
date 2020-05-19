ed9477fcbbffd7dbd799a30b67946e7a54d64b69
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-19 20:21:50 +0100

    first commit on new dev branch

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import css from './gameBoard.module.css';
-import apiUrl from '../../libs/config';
 
 import BoardColumn from '../BoardColumn';
 
